d11317707e32d1529fe619849b43f3bddd1a205b
AmanKamani <kamaniamanak@gmail.com>
2022-04-03 12:02:20 +0530

    feat(shopping-list-module): Separated ShoppingList Module.
    - Added exports in shopping-list.module.ts to access the shopping list components in app-routing module.
    
    Signed-off-by: AmanKamani <kamaniamanak@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
-import {ShoppingListComponent} from './feature-shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './feature-shopping-list/shopping-edit/shopping-edit.component';
 import {ShoppingListService} from "./feature-shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -17,13 +15,12 @@ import {AuthInterceptor} from "./auth/auth-interceptor.service";
 import {AlertComponent} from "./shared/alert/alert.component";
 import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
 import {RecipesModule} from "./feature-recipes/recipes.module";
+import {ShoppingListModule} from "./feature-shopping-list/shopping-list.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -36,6 +33,7 @@ import {RecipesModule} from "./feature-recipes/recipes.module";
     ReactiveFormsModule,
     AppRoutingModule,
     RecipesModule,
+    ShoppingListModule,
   ],
   providers: [
     ShoppingListService,
---
+++ src/app/feature-shopping-list/shopping-list.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {ShoppingListComponent} from "./shopping-list.component";
+import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
+  exports: [
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RouterModule,
+  ]
+})
+export class ShoppingListModule {
+
+}
