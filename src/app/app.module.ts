00af963008818552f35662f82386cfff6eb784c1
AmanKamani <kamaniamanak@gmail.com>
2022-04-03 12:47:56 +0530

    feat(auth-module): Separated auth module.
    - Moved auth routing & Component to the auth.module.ts. And imported in app.module.ts
    
    Signed-off-by: AmanKamani <kamaniamanak@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,27 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {AppRoutingModule} from "./app-routing.module";
-import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {AuthComponent} from './auth/auth.component';
 import {RecipesModule} from "./feature-recipes/recipes.module";
 import {ShoppingListModule} from "./feature-shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
+    AuthModule,
   ],
   bootstrap: [AppComponent]
 })
